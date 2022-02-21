c91288495374694dc977d2ff7c6a03512b51c8b8
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-21 15:34:21 +0500

    add views file

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Order
 # Create your views here.
+def firs_page(request):
+    object_list = Order.objects.all() # mdul ichida hamma queriset malumotlarni ol diyapmiz
+    context = {'object_list':object_list}
+    return render(request, 'index.html',context)
