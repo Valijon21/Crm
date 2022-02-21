e24a3f97145fef48a7bc0bb545c5097b919c08b6
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-21 15:21:12 +0500

    add model admin panel

--- crm/models.py
+++ crm/models.py
@@ -3,6 +3,13 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     order_dt = models.DateField(auto_now=True)
-    ored_name = models.CharField(max_length=200)
-    oder_phone = models.CharField(max_length=200)
+    order_name = models.CharField(max_length=200,verbose_name='ismi')
+    order_phone = models.CharField(max_length=200,verbose_name='telefon')
     
+    
+    def __str__(self):
+        return self.order_name
+    class Meta:
+        verbose_name = 'zakaz'
+        verbose_name_plural = 'zakazlar'
+        
