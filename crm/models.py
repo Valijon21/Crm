3a0c522377caab3ad65789ee592c31bf8d18362b
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-21 14:53:02 +0500

    Add models fields

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    order_dt = models.DateField(auto_now=True)
+    ored_name = models.CharField(max_length=200)
+    oder_phone = models.CharField(max_length=200)
+    
