5c68c01f38070c3884d913090032c9d842e25bcc
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-22 05:47:05 +0500

    bush forma qilish required = Fasle

--- crm/forms.py
+++ crm/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class Orderform(forms.Form):
     
-    name = forms.CharField(max_length=200)
+    name = forms.CharField(max_length=200,required = False) # required =False qilsak kerakmas polyani tuldirmasdan junatish m-n
     phone = forms.CharField(max_length=200)
     
